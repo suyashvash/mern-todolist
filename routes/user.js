25ca3356a1fde642adfc7f20589ad318a2c3e3ee
Suyash Vashishtha <suyashvashishtha@gmail.com>
2023-09-11 00:01:18 +0530

    User Fix

--- routes/user.js
+++ routes/user.js
@@ -8,7 +8,7 @@ userRouter.route('/signup').post((req, res) => {
     const username = req.body.username;
     const password = req.body.password;
     
-    const newUser = new Task({ username, password });
+    const newUser = new User({ username, password });
 
     newUser.save()
         .then(() => res.json('User added!'))
