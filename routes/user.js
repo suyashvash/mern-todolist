388f743fe1bacf71e56b089a61e8dbda2501b411
Suyash Vashishtha <suyashvashishtha@gmail.com>
2023-09-10 23:56:54 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,7 @@ import User from '../models/user.model.js';
 
 const userRouter = Router();
 
-userRouter.route('/signup').get((req, res) => {
+userRouter.route('/signup').post((req, res) => {
     const username = req.body.username;
     const password = req.body.password;
     
