a24cab2ba958b75f78b810fb0229416b2e20ccd4
Suyash Vashishtha <suyashvashishtha@gmail.com>
2023-09-10 23:33:54 +0530

    Added User

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import userRouter from './routes/user.js';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5001
 
 app.use(cors())
 app.use(express.json());
